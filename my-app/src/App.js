d74c5321dc2d23a9c51f6ef0f0bcd6c59f5318d7
Anna Skvortsova <annascvo@gmail.com>
2022-09-18 16:45:19 +0300

    Task 3: Получение  ряда Фибоначчи

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,26 @@
+import { useState } from "react";
+import Item from "./components/Item";
+
 export default function App() {
+  const [numbers, setNumbers] = useState([0, 1, 1]);
+
+  const getNextNumber = () => {
+    const nextNumber = numbers[numbers.length - 1] + numbers[numbers.length - 2];
+    setNumbers([...numbers, nextNumber]);
+  }
+
   return (
     <div className="container">
       <div className="slider">
         <div className="item-list">
-          <div className="item">
-            <p className="number">0</p>
-          </div>
-          <div className="item">
-            <p className="number">1</p>
-          </div>
-          <div className="item">
-            <p className="number">1</p>
-          </div>
+          {numbers.length > 0 && numbers.map((item,index) => {
+            return (<Item key={index} number={item} styleClass={"color"} />)
+          })
+          }
         </div>
         <div className="navigation">
           <button className="arrow">-</button>
-          <button className="arrow">+</button>
+          <button className="arrow" onClick={getNextNumber}>+</button>
         </div>
       </div>
     </div>
