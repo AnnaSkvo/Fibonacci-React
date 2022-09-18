ec5a2216a1bdd7a3aa3dd98f23ed7d6ced622ad1
Anna Skvortsova <annascvo@gmail.com>
2022-09-18 18:11:06 +0300

    Task 6: Стилизация цвета карточек + логика

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,34 @@
 import { useState, useCallback, useEffect } from "react";
 import Item from "./components/Item";
 
+const colorList = ["one", "two", "three", "four", "five"];
+
 export default function App() {
   const [numbers, setNumbers] = useState([0, 1, 1]);
   const [number, setNumber] = useState(1);
+  const [colors, setColors] = useState(["one", "two", "three"]);
 
   const getNextNumber = () => {
     setNumber(number + 1);
+    const newColor = getRandomColor();
+    setColors([...colors.slice(1), newColor]);
   }
 
   const getPrewNumber = () => {
     if (number > 1) {
       setNumber(number - 1);
+      const newColor = getRandomColor();
+      setColors([newColor, ...colors.slice(0, -1)]);
     }
   }
+
+  const getEmptyColors = () => {
+    return colorList.filter((color) => !colors.includes(color));
+  };
+
+  const getRandomColor = () => {
+    return getEmptyColors()[Math.round(Math.random())];
+  };
 
   const setArray = useCallback(() => {
     const cache = [0, 1];
@@ -25,7 +40,7 @@ export default function App() {
     if (number % 2 === 0) {
       cache.reverse();
     }
-    
+
     setNumbers([...cache, cache[0] + cache[1]]);
   }, [number]);
 
@@ -38,7 +53,7 @@ export default function App() {
       <div className="slider">
         <div className="item-list">
           {numbers.length > 0 && numbers.map((item, index) => {
-            return (<Item key={index} number={item} styleClass={"color"} />)
+            return (<Item key={index} number={item} styleClass={colors[index]} />)
           })
           }
         </div>
