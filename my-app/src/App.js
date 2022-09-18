5d428d4f2f566686d03fa77ed4041387f99af9c3
Anna Skvortsova <annascvo@gmail.com>
2022-09-18 15:40:50 +0300

    Task 2: Верстка страницы

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,23 @@
-function App() {
+export default function App() {
   return (
-    <div className="App"></div>
+    <div className="container">
+      <div className="slider">
+        <div className="item-list">
+          <div className="item">
+            <p className="number">0</p>
+          </div>
+          <div className="item">
+            <p className="number">1</p>
+          </div>
+          <div className="item">
+            <p className="number">1</p>
+          </div>
+        </div>
+        <div className="navigation">
+          <button className="arrow">-</button>
+          <button className="arrow">+</button>
+        </div>
+      </div>
+    </div>
   );
 }
-
-export default App;
