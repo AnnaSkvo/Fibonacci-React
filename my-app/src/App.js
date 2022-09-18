2a08e6efc7398168c4772d4ac5b0d1d84521ef6a
Anna Skvortsova <annascvo@gmail.com>
2022-09-18 17:19:33 +0300

    Task 5: Переписала логику для экономии памяти

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,43 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Item from "./components/Item";
 
 export default function App() {
   const [numbers, setNumbers] = useState([0, 1, 1]);
+  const [number, setNumber] = useState(1);
 
   const getNextNumber = () => {
-    const nextNumber = numbers[numbers.length - 1] + numbers[numbers.length - 2];
-    setNumbers([...numbers, nextNumber]);
+    setNumber(number + 1);
   }
 
   const getPrewNumber = () => {
-    if(numbers.length > 3){
-      setNumbers([...numbers.slice(0, numbers.length - 1)]);
+    if (number > 1) {
+      setNumber(number - 1);
     }
   }
+
+  const setArray = useCallback(() => {
+    const cache = [0, 1];
+
+    for (let i = 2; i <= number; i++) {
+      cache[i % 2] = cache[0] + cache[1];
+    }
+
+    if (number % 2 === 0) {
+      cache.reverse();
+    }
+    
+    setNumbers([...cache, cache[0] + cache[1]]);
+  }, [number]);
+
+  useEffect(() => {
+    setArray();
+  }, [number, setArray]);
 
   return (
     <div className="container">
       <div className="slider">
         <div className="item-list">
-          {numbers.length > 0 && numbers.slice(-3).map((item,index) => {
+          {numbers.length > 0 && numbers.map((item, index) => {
             return (<Item key={index} number={item} styleClass={"color"} />)
           })
           }
