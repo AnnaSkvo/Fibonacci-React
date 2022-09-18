35a0d9b2672e1dd656439438d43b62e6963f8151
Anna Skvortsova <annascvo@gmail.com>
2022-09-18 16:55:32 +0300

    Task 5: Логика "назад" и ограничение вывода чисел

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,17 +9,23 @@ export default function App() {
     setNumbers([...numbers, nextNumber]);
   }
 
+  const getPrewNumber = () => {
+    if(numbers.length > 3){
+      setNumbers([...numbers.slice(0, numbers.length - 1)]);
+    }
+  }
+
   return (
     <div className="container">
       <div className="slider">
         <div className="item-list">
-          {numbers.length > 0 && numbers.map((item,index) => {
+          {numbers.length > 0 && numbers.slice(-3).map((item,index) => {
             return (<Item key={index} number={item} styleClass={"color"} />)
           })
           }
         </div>
         <div className="navigation">
-          <button className="arrow">-</button>
+          <button className="arrow" onClick={getPrewNumber}>-</button>
           <button className="arrow" onClick={getNextNumber}>+</button>
         </div>
       </div>
